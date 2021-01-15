08866e1ffbca03b75b92112dc73fafb4b0678526
KaimNotark <kaimnotark@yandex.ru>
2021-01-15 09:35:05 +0800

    finish news-app

--- projects/news-app.js
+++ projects/news-app.js
@@ -75,14 +75,24 @@ const newsService = (function () {
   const apiUrl = 'https://news-api-v2.herokuapp.com';
 
   return {
-    topHeadlines(country = 'ua', cb) {
-      http.get(`${apiUrl}/top-headlines?country=${country}&category=technology&apiKey=${apiKey}`, cb);
+    topHeadlines(country = 'ua', category = '', cb) {
+      http.get(`${apiUrl}/top-headlines?country=${country}&category=${category}&apiKey=${apiKey}`, cb);
     },
     everything(query, cb) {
       http.get(`${apiUrl}/everything?q=${query}&apiKey=${apiKey}`, cb);
     }
   };
 })();
+
+const form = document.forms['newsControls'];
+const countrySelect = form.elements['country'];
+const categorySelect = form.elements['category'];
+const searchInput = form.elements['search'];
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  loadNews();
+});
 
 //  init selects
 document.addEventListener('DOMContentLoaded', function () {
@@ -91,19 +101,45 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 function loadNews() {
-  newsService.topHeadlines('ua', onGetResponse);
+  showPreloader();
+
+  const country = countrySelect.value;
+  const category = categorySelect.value;
+  const searchText = searchInput.value;
+
+  if (!searchText) {
+    newsService.topHeadlines(country, category, onGetResponse);
+  } else {
+    newsService.everything(searchText, onGetResponse);
+  }
 };
 
 function onGetResponse(err, res) {
-  console.log('onGetResponse', res.articles);
+
+  removePreloader();
+
+  if (err) {
+    showAlert(err, 'error-msg');
+    return;
+  };
+
+  if (!res.articles.length) {
+    // show empty message
+    return;
+  };
+
   renderNews(res.articles);
 };
 
 function renderNews(news) {
   const newsContainer = document.querySelector('.news-container .row');
+  if (newsContainer.children.length) {
+    clearContainer(newsContainer);
+  };
   let fragment = '';
 
   news.forEach(newsItem => {
+    console.log(newsItem.urlToImage);
     const el = newsTemplate(newsItem);
     fragment += el;
   });
@@ -120,7 +156,7 @@ function newsTemplate({ urlToImage, title, description, url }) {
       <img src="${urlToImage}">
       <span class="card-title">${title || ''}</span>
     </div>
-    <div class="card-content">  
+    <div class="card-content-news">  
       <p>${description || ''}</p>  
     </div>  
     <div class="card-action">
@@ -129,4 +165,42 @@ function newsTemplate({ urlToImage, title, description, url }) {
   </div>
 </div>
 `;
+};
+
+// создание всплывающего сообщения об ошибке
+// применяют готовое решение из фреймворка
+// возможно настроить это сообщение, настройки см. на сайте фреймворка
+function showAlert(msg, type = "success") {
+  M.toast({ html: msg, classes: type });
+};
+
+// очистка страницы перед добавлением новых новостей
+// описаны два способа, первый закомментирован
+function clearContainer(container) {
+
+  // container.innerHTML='';
+
+  let child = container.lastElementChild;
+  while (child) {
+    container.removeChild(child);
+    child = container.lastElementChild;
+  };
+};
+
+// прелоадер (индикатор загрузки = прогресс-индикатор)
+// тоже берётся из фреймворка
+function showPreloader() {
+  document.body.insertAdjacentHTML('afterbegin',
+    `
+  <div class="progress">
+    <div class="indeterminate">
+    </div>
+  </div>
+`
+  );
+};
+
+function removePreloader() {
+  const loader = document.querySelector('.progress');
+  if (loader) { loader.remove(); };
 };
